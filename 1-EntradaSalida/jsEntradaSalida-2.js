955a5830c8cf56c8a817f52dfcc2c04fdfcc0caa
Ferré Nicolás <36193611+ferreenicolas@users.noreply.github.com>
2018-02-06 10:16:52 -0300

    Ejercicio (1-2)
    prompt, alert, comentarios, crtl s constante, concatenar, bloque

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,23 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
+	var eq; 
+	nombre = prompt("Ingrese su nombre","Natalia Natalia");
+	alert("Su nombre es: "+nombre);
+//prompt devuelve algo, concatenar (encadenar)
+//siempre con una variable 
+
+
+	// esto es un comentario de una linea
+
+	//alert(nombre);
+	/*
+	esto es un bloque 
+	alert("nombre");
+	nombre = "Ferré Nicolás";
+	alert(nombre);
+	*/
+	//var person = prompt("Please enter your name", "Harry Potter");
 	
 }
 
