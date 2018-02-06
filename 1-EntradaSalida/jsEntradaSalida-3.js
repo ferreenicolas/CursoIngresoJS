262adea0c04f519f58fc2bec77ec9fb78cb52dc3
Ferré Nicolás <36193611+ferreenicolas@users.noreply.github.com>
2018-02-06 11:59:04 -0300

    3,4,5
    prompt, concatenar, variables, asignaciones d las mismas

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -3,6 +3,12 @@ y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
 	//alert("esto anda");
-	//document.g tab (shift+i) 
-	document.getElementById('')
+	//document.g tab(agarrar elemento) (por id: shift+i)
+	var nombre;
+	nombre = document.getElementById('elNombre').value;
+	alert(nombre);
+    // si dice objecthtml falta el .value 
+
+
+	}
 
