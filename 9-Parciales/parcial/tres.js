1fa3b8abed13725a567b111b2c5581b62e1885a3
Ferré Nicolás <36193611+ferreenicolas@users.noreply.github.com>
2018-02-07 14:38:06 -0300

    3 +html corregido

--- 9-Parciales/parcial/tres.js
+++ 9-Parciales/parcial/tres.js
@@ -1,4 +1,17 @@
 function Mostrar()
 {
 
+	//variables
+	var largo
+	var ancho
+	var resultadolados
+	//asignaciones
+	largo=document.getElementById('largo').value;
+	ancho=document.getElementById('ancho').value;
+	//cuentas
+	resultadolados= largo * ancho / 4 *3
+	//final
+	alert(resultadolados)
+
+
 }
