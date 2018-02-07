45f87fa02567f9bdf6b0dfbb8773b253fd3d625b
Ferré Nicolás <36193611+ferreenicolas@users.noreply.github.com>
2018-02-07 12:20:17 -0300

    4

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,16 @@ function Mostrar()
 {
 //tomo la edad  
 
+var edad
 
+edad=document.getElementById('edad').value;
+
+if(edad>12)
+{
+	if(edad<19)
+	{
+		alert("Es adolescente");
+	}
+}
 
 }//FIN DE LA FUNCIÓN
