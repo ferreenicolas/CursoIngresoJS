8e744c758bbe883d05b542327928584f8d1f5f9f
Ferré Nicolás <36193611+ferreenicolas@users.noreply.github.com>
2018-02-07 09:59:16 -0300

    8 y 7 e/s

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -3,22 +3,74 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
-{	
-	
+{
+	var numeroUno;
+    var numeroDos;
+    var resultado;
+
+
+
+    numeroUno= document.getElementById('numeroUno').value;
+    numeroDos= document.getElementById('numeroDos').value;
+    numeroUno= parseInt(numeroUno);
+    numeroDos= parseInt(numeroDos);
+
+    resultado= numeroUno+numeroDos;
+
+    alert("la Suma es "+resultado);
 }
 
 function restar()
 {
-	
+	var numeroUno;
+    var numeroDos;
+    var resultado;
+
+
+
+    numeroUno= document.getElementById('numeroUno').value;
+    numeroDos= document.getElementById('numeroDos').value;
+    numeroUno= parseInt(numeroUno);
+    numeroDos= parseInt(numeroDos);
+
+    resultado= numeroUno - numeroDos;
+
+    alert("la Resta es "+resultado);
 }
 
 function multiplicar()
 { 
-	
+	var numeroUno;
+    var numeroDos;
+    var resultado;
+
+
+
+    numeroUno= document.getElementById('numeroUno').value;
+    numeroDos= document.getElementById('numeroDos').value;
+    numeroUno= parseInt(numeroUno);
+    numeroDos= parseInt(numeroDos);
+
+    resultado= numeroUno * numeroDos;
+
+    alert("la Multiplicacion es "+resultado);
 }
 
 function dividir()
 {
-	
+	var numeroUno;
+    var numeroDos;
+    var resultado;
+
+
+
+    numeroUno= document.getElementById('numeroUno').value;
+    numeroDos= document.getElementById('numeroDos').value;
+    numeroUno= parseInt(numeroUno);
+    numeroDos= parseInt(numeroDos);
+
+    resultado= numeroUno / numeroDos;
+
+    alert("la Division es "+resultado);
 }
 
