ae7662e4c612b9ee8a6c5ce4eb22978bca5cc297
Ferré Nicolás <36193611+ferreenicolas@users.noreply.github.com>
2018-02-06 22:58:52 -0300

    2
    intento fallido

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,9 @@
 function Mostrar()
 {
-  
+  var importefinal;
+  var importeproducto;
+
+  importeproducto= prompt("Ingrese el importe del producto","Producto");
+  importefinal=document.getElementById('importeFinal').value
+
 }
