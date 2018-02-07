9c89481b51fc059fda5a34b05399e83836f07d12
Ferré Nicolás <36193611+ferreenicolas@users.noreply.github.com>
2018-02-07 11:28:00 -0300

    10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,20 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
+	var importe;
+	var resultado;
+	var descuento25;
 	
+	
+
+	importe=document.getElementById('importe').value;
+	
+	importe=parseInt(importe);
+	resultado=parseInt(resultado);
+
+	descuento25= importe * 0.25;
+	resultado= importe - descuento25;
+	
+
+	document.getElementById('resultado').value=resultado;
 }
