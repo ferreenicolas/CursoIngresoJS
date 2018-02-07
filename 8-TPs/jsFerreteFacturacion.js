c377cf5e0765f26b3ce74788bf20b81402409673
Ferré Nicolás <36193611+ferreenicolas@users.noreply.github.com>
2018-02-06 22:59:36 -0300

    ferretefacturacion
    casi completado, falta iva

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -8,16 +8,51 @@ function Sumar ()
 	var precio1;
 	var precio2;
 	var precio3;
-precio1= document.getElementById('PrecioUno');
-precio2= document.getElementById('PrecioDos')
-precio3= document.getElementById('PrecioTres')
+	var resultado;
+
+precio1= parseInt(document.getElementById('PrecioUno').value);
+precio2= parseInt(document.getElementById('PrecioDos').value);
+precio3= parseInt(document.getElementById('PrecioTres').value);
+resultado= precio1+ precio2+ precio3;
+
+alert(resultado);
+
 
 }
 function Promedio () 
 {
-	
+	var precio1;
+	var precio2;
+	var precio3;
+	var resultado1;
+	var resultadofinal;
+
+
+precio1= parseInt(document.getElementById('PrecioUno').value);
+precio2= parseInt(document.getElementById('PrecioDos').value);
+precio3= parseInt(document.getElementById('PrecioTres').value);
+resultado1= precio1+ precio2+ precio3;
+resultadofinal= resultado1 / 3;
+
+alert(resultadofinal)
 }
 function PrecioFinal () 
 {
-	
+	var precio1;
+	var precio2;
+	var precio3;
+	var resultadoneto;
+	var resultado21;
+	var resultadoiva;
+
+
+precio1= parseInt(document.getElementById('PrecioUno').value);
+precio2= parseInt(document.getElementById('PrecioDos').value);
+precio3= parseInt(document.getElementById('PrecioTres').value);
+resultadoneto= precio1+ precio2+ precio3;
+resultado21= resultadoneto *21 /100 ;
+//
+
+alert(resultado21);
+
 }
