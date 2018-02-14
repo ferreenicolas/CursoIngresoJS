21514b32e68407d4f5134ba532f8415b5ccd61c4
Ferré Nicolás <36193611+ferreenicolas@users.noreply.github.com>
2018-02-14 12:26:06 -0300

    hasta el 5 inclusive

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,38 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+	switch(mesDelAño)
+	{
+		case "Julio":
+		case "Agosto":
+
+		alert("Abrigate que hace frio.");
+
+			break;
+		case "Septiembre":
+		case "Octubre":
+		case "Noviembre":
+		case "Diciembre":
+
+
+		alert("Ya pasamos el frio, ahora calor!!!.");
+
+			break;
+		default:
+			alert("Falta para el invierno.");
+	}
+
+
+
+
+
+
+
+
+
+
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,9 +1,30 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
+	//alert (mesDelAño);
+	// enero 31, feb 28, mar31,ab30,may31,jun30
+	//jul31,agos31,sep30,oct31,nov30,dic31
+
+	switch(mesDelAño)
+	{
+		case "Febrero":
+		alert("Tiene 28.");
+			break;
+
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+		alert("Tiene 30 días.");
+			break;
+
+		default:
+		alert("Tiene 31 días.");
+		
+		
+	}	
 	
 	
 
