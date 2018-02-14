aa921ad39526a8b6648658fefb5cad69a78f862a
Ferré Nicolás <36193611+ferreenicolas@users.noreply.github.com>
2018-02-14 11:02:37 -0300

    9,10

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,27 @@
 function Mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
+//Genero el número RANDOM entre 1 y 10 
+	var numeroRandom;
+
+	numeroRandom = Math.floor((Math.random() * 10) + 1);
+
+	console.log(numeroRandom);
+
+	if(numeroRandom > 8)
+	{
+		alert("EXCELENTE");
+	}
+	else
+	{	
+		if(numeroRandom > 3)
+		{
+			alert("APROBÓ");
+		}
+		else
+		{
+				alert("Vamos, la próxima se puede");
+		}
+	}
+
 
 }//FIN DE LA FUNCIÓN
