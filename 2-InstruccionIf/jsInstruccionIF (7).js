b7f04558ddc2a37546d46561c72f03d1b0bceb02
Ferré Nicolás <36193611+ferreenicolas@users.noreply.github.com>
2018-02-14 09:20:30 -0300

    7,8
    if, cuadro de texto con valores predeterminados.

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,22 @@
 function Mostrar()
 {
 //tomo la edad  
+var estadoCivil= document.getElementById('estadoCivil').value;
+var edad= document.getElementById('edad').value;
 
-	
+if(edad<18 && estadoCivil != "Soltero")
+{
+	alert("Es muy pequeño para NO ser soltero.");
+}
 
+
+// ! niega la condicion 
+// == comparacion - mismo tipo
+
+/*
+"Soltero"	
+"Casado"
+"Divorciado"
+*/
 
 }//FIN DE LA FUNCIÓN
