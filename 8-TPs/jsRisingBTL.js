9031a12b03f05c2903c195943c66e36549378b14
Ferré Nicolás <36193611+ferreenicolas@users.noreply.github.com>
2018-02-28 12:06:24 -0300

    Add files via upload

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,5 +9,87 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
+ 	var edad;
+ 	var sexo;
+ 	var estadoCivil;
+ 	var sueldo;
+ 	var legajo;
+ 	var nacionalidad;
+ 	var sexof = 0;
+ 	var sexom = 0;
+ 	var soltero = 1;
+ 	var casados= 2;
+ 	var divorciados = 3;
+ 	var viudos = 4;
+ 	var argentinos = "a"
+ 	var extranjeros = "e"
+ 	var nacionalizados = "n"
+
  
+ 		edad=prompt("Ingrese su edad.");
+ 		edad=parseInt(edad);
+ 		while(edad < 18 || edad > 90 || isNaN(edad))
+ 		{
+ 			alert("Edad invalida.");
+ 			edad=prompt("Ingrese su edad.");
+ 			edad=parseInt(edad);
+ 		}
+		sexo=prompt("Ingresar sexo.");
+		while(sexo != "f" && sexo != "m" )
+		{
+			alert("Sexo invalido.");
+			sexo=prompt("Ingresar sexo.");
+		}
+		if(sexo== "f" )
+		{
+			sexo = "Femenino"
+		}
+		else
+		{
+			if(sexo == "m")	
+			{
+				sexo = "Masculino"
+			}
+		}
+
+ 		estadoCivil= prompt("Ingresar su estado civil.");
+		while(estadoCivil != soltero && estadoCivil != casados && estadoCivil!= divorciados && estadoCivil != viudos)
+		{
+			alert("Estado civil invalido, ingrese 1 = soltero, 2= casado, 3= divorciado y 4= viudo.")
+			estadoCivil = prompt("Ingresar su estado civil.");
+		}
+		switch(estadoCivil)
+
+		
+		sueldo=prompt("Ingresar sueldo bruto (no menor a 8000).");
+		sueldo=parseInt(sueldo);
+		while(sueldo < 8000)
+		{
+			sueldo=prompt("Ingresar sueldo bruto (no menor a 8000).");
+			sueldo=parseInt(sueldo);
+		}
+
+ 		legajo= prompt("Ingrese el legajo.");
+ 		
+ 		while(legajo > 9999 || legajo < 1000)
+ 		{
+ 			alert("Legajo invalido.");
+ 			legajo= prompt("Ingrese el legajo.");
+ 		}
+ 		
+ 		nacionalidad=prompt("Ingresar nacionalidad.");
+ 		
+ 		while(nacionalidad != argentinos && nacionalidad != extranjeros && nacionalidad != nacionalizados)
+ 		{
+ 			alert("Nacionalidad invalida.");
+ 			nacionalidad= prompt("Ingresar nacionalidad.");
+ 		}
+	 	alert("Edad: "+edad);
+	 	alert("Sexo: "+sexo);
+	 	alert("Estado Civil: "+estadoCivil);	
+		alert("Sueldo: "+sueldo);
+		alert("Legajo: "+legajo);
+		alert("Nacionalidad: "+nacionalidad);
 }
+ 
+
